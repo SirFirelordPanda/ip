a9c74c1cb7a95d96463d58106f79bd29bc5e83e4
SirFirelordPanda <mismailmsalim@gmail.com>
2026-01-21 19:57:40 +0800

    Renamed Duke to Misora, added greeting and exit

---
+++ src/main/java/Misora.java
@@ -0,0 +1,22 @@
+public class Misora {
+    public static void main(String[] args) {
+        String logo =   "   _____  .___  _________________ __________    _____\n" +
+                        "  /     \\ |   |/   _____/\\_____  \\______    \\  /  _  \\\n" +
+                        " /  \\ /  \\|   |\\_____  \\  /   |   \\|       _/ /  /_\\  \\\n" +
+                        "/    Y    \\   |/        \\/    |    \\    |   \\/    |    \\\n" +
+                        "\\____|__  /___/_______  /\\_______  /____|_  /\\____|__  /\n" +
+                        "        \\/            \\/         \\/       \\/         \\/";
+
+        String breakerLine = "____________________________________________________________";
+
+        String greeting = "Hello! Im MISORA.\nWhat can I do for you?";
+
+        String exit = "Bye. Hope to see you again soon!\n";
+
+        System.out.println(logo);
+        System.out.println(breakerLine);
+        System.out.println(greeting);
+        System.out.println(breakerLine);
+        System.out.println(exit);
+    }
+}
