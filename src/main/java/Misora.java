f1b0a589e7693ed0e9fc3b0cd4b8ee2c61b67667
SirFirelordPanda <mismailmsalim@gmail.com>
2026-01-21 22:53:26 +0800

    Echo function : chatbot echoes wtv is said by user
    chatbot terminates when bye is typed in case-insensitive

--- src/main/java/Misora.java
+++ src/main/java/Misora.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Misora {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         String logo =   "   _____  .___  _________________ __________    _____\n" +
                         "  /     \\ |   |/   _____/\\_____  \\______    \\  /  _  \\\n" +
                         " /  \\ /  \\|   |\\_____  \\  /   |   \\|       _/ /  /_\\  \\\n" +
@@ -9,13 +13,28 @@ public class Misora {
 
         String breakerLine = "____________________________________________________________";
 
-        String greeting = "Hello! Im MISORA.\nWhat can I do for you?";
+        String greeting = "Nyahallo! Im MISORA.\nWhat can I do for you?";
 
-        String exit = "Bye. Hope to see you again soon!\n";
+        String exit = "Bye bye. Hope to see you again soon!\n";
 
         System.out.println(logo);
         System.out.println(breakerLine);
         System.out.println(greeting);
+
+        while (true) {
+
+            System.out.println(breakerLine);
+            String inputLine = scanner.nextLine();
+
+            if (inputLine.equalsIgnoreCase("bye")) {
+                break;
+            }
+
+            System.out.println(breakerLine);
+            System.out.println(inputLine);
+        }
+
+        scanner.close();
         System.out.println(breakerLine);
         System.out.println(exit);
     }
