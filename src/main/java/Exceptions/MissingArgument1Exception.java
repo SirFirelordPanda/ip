2776531a0d08b0ebcc88b2f9a091ec0cdbcde41a
SirFirelordPanda <mismailmsalim@gmail.com>
2026-01-22 18:06:15 +0800

    Error-Handling
    Incorrect inputs will now trigger error messages with the correct
    instructions

---
+++ src/main/java/Exceptions/MissingArgument1Exception.java
@@ -0,0 +1,4 @@
+package Exceptions;
+
+public class MissingArgument1Exception extends Exception{
+}
--- src/main/java/Misora.java
+++ src/main/java/Misora.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import Exceptions.*;
 
 public class Misora {
     public static void main(String[] args) {
@@ -71,59 +72,115 @@ public class Misora {
                 }
 
             } else if (inputLine.toLowerCase().startsWith("todo ")) {
-
-                System.out.println(breakerLine);
-                if (listLength < 99) {
-                    ToDo todo = new ToDo(inputLine.substring(5).trim());
-                    listOfTasks[listLength] = todo;
-                    listLength++;
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println(todo.toString());
-                    System.out.printf("Now you have %d tasks in the list.\n", listLength);
+                try {
+                    System.out.println(breakerLine);
+                    String taskMsg = inputLine.substring(5).trim();
+                    if (taskMsg.isEmpty()) {
+                        throw new MissingTaskMsgException();
+                    }
+                    if (listLength < 100) {
+                        ToDo todo = new ToDo(taskMsg);
+                        listOfTasks[listLength] = todo;
+                        listLength++;
+                        System.out.println("Got it. I've added this task:");
+                        System.out.println(todo.toString());
+                        System.out.printf("Now you have %d tasks in the list.\n", listLength);
+                    } else {
+                        throw new TooManyTasksException();
+                    }
+                } catch (TooManyTasksException e) {
+                    System.out.println("WHOOPSIE!! You have too many tasks in the task list");
+                } catch (MissingTaskMsgException e) {
+                    System.out.println("WHOOPSIE!! Please enter the description of the task in this format 'todo -taskMsg-'");
                 }
 
             } else if (inputLine.toLowerCase().startsWith("deadline ")) {
 
-                System.out.println(breakerLine);
-                String byWhen = "";
-                String taskMsg;
-                int byIndex = inputLine.indexOf("/by");
-                if (byIndex != -1) {
-                    byWhen = inputLine.substring(byIndex + 3).trim();
-                    taskMsg = inputLine.substring(9, byIndex).trim();
-                } else {
-                    taskMsg = inputLine;
-                }
-                if (listLength < 99) {
-                    Deadline deadline = new Deadline(taskMsg, byWhen);
-                    listOfTasks[listLength] = deadline;
-                    listLength++;
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println(deadline.toString());
-                    System.out.printf("Now you have %d tasks in the list.\n", listLength);
+                try {
+                    System.out.println(breakerLine);
+                    String byWhen = "";
+                    String taskMsg;
+                    int byIndex = inputLine.indexOf("/by");
+                    if (byIndex != -1) {
+                        byWhen = inputLine.substring(byIndex + 3).trim();
+                        taskMsg = inputLine.substring(9, byIndex).trim();
+                        if (taskMsg.isEmpty()) {
+                            throw new MissingTaskMsgException();
+                        } else if (byWhen.isEmpty()) {
+                            throw new MissingArgument1Exception();
+                        }
+                    } else {
+                        throw new MissingArgument1Exception();
+                    }
+                    if (listLength < 100) {
+                        Deadline deadline = new Deadline(taskMsg, byWhen);
+                        listOfTasks[listLength] = deadline;
+                        listLength++;
+                        System.out.println("Got it. I've added this task:");
+                        System.out.println(deadline.toString());
+                        System.out.printf("Now you have %d tasks in the list.\n", listLength);
+                    } else {
+                        throw new TooManyTasksException();
+                    }
+                } catch (TooManyTasksException e) {
+                    System.out.println("WHOOPSIE!! You have too many tasks in the task list");
+                } catch (MissingTaskMsgException e) {
+                    System.out.println("WHOOPSIE!! Please enter the description of the task in this format\n" +
+                            "'deadline -taskMsg- /by -byWhen-'");
+                } catch (MissingArgument1Exception e) {
+                    System.out.println("WHOOPSIE!! Please enter the deadline of the task in this format\n" +
+                            "'deadline -taskMsg- /by -byWhen-'");
                 }
             } else if (inputLine.toLowerCase().startsWith("event ")) {
+                try {
+                    System.out.println(breakerLine);
+                    String fromWhen = "";
+                    String toWhen = "";
+                    String taskMsg = "";
+                    int fromIndex = inputLine.indexOf("/from");
+                    int toIndex = inputLine.indexOf("/to");
+                    if (fromIndex != -1 && toIndex != -1) {
+                        fromWhen = inputLine.substring(fromIndex + 5, toIndex).trim();
+                        toWhen = inputLine.substring(toIndex + 3).trim();
+                        taskMsg = inputLine.substring(6, fromIndex).trim();
+                        if (taskMsg.isEmpty()) {
+                            throw new MissingTaskMsgException();
+                        } else if (fromWhen.isEmpty()) {
+                            throw new MissingArgument1Exception();
+                        } else if (toWhen.isEmpty()) {
+                            throw new MissingArgument2Exception();
+                        }
+                    } else if (fromIndex == -1){
+                        throw new MissingArgument1Exception();
+                    } else {
+                        throw new MissingArgument2Exception();
+                    }
+                    if (listLength < 99) {
+                        Event event = new Event(taskMsg, fromWhen, toWhen);
+                        listOfTasks[listLength] = event;
+                        listLength++;
+                        System.out.println("Got it. I've added this task:");
+                        System.out.println(event.toString());
+                        System.out.printf("Now you have %d tasks in the list.\n", listLength);
+                    } else {
+                        throw new TooManyTasksException();
+                    }
+                } catch (TooManyTasksException e) {
+                    System.out.println("WHOOPSIE!! You have too many tasks in the task list");
+                } catch (MissingTaskMsgException e) {
+                    System.out.println("WHOOPSIE!! Please enter the description of the event in this format\n" +
+                            "'event -taskMsg- /from -fromWhen- /to -toWhen-'");
+                } catch (MissingArgument1Exception e) {
+                    System.out.println("WHOOPSIE!! Please enter the start time of the event in this format\n" +
+                            "'event -taskMsg- /from -fromWhen- /to -toWhen-'");
+                } catch (MissingArgument2Exception e) {
+                    System.out.println("WHOOPSIE!! Please enter the end time of the event in this format\n" +
+                            "'event -taskMsg- /from -fromWhen- /to -toWhen-'");
+                }
+            } else {
                 System.out.println(breakerLine);
-                String fromWhen = "";
-                String toWhen = "";
-                String taskMsg = "";
-                int fromIndex = inputLine.indexOf("/from");
-                int toIndex = inputLine.indexOf("/to");
-                if (fromIndex != -1 && toIndex != -1) {
-                    fromWhen = inputLine.substring(fromIndex + 5, toIndex).trim();
-                    toWhen = inputLine.substring(toIndex + 3).trim();
-                    taskMsg = inputLine.substring(6, fromIndex).trim();
-                } else {
-                    taskMsg = inputLine;
-                }
-                if (listLength < 99) {
-                    Event event = new Event(taskMsg, fromWhen, toWhen);
-                    listOfTasks[listLength] = event;
-                    listLength++;
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println(event.toString());
-                    System.out.printf("Now you have %d tasks in the list.\n", listLength);
-                }
+                System.out.println("Uh oh stinky, I was not programmed to handle wtv you just typed in.\n" +
+                        "WHOOPSIE!! Maybe you can try again in the future when this is updated");
             }
         }
 
